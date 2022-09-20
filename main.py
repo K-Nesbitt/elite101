d1d8d8b6d02a00a126cfa855eea2e5f1e5669685
Keatra Nesbitt <49926788+K-Nesbitt@users.noreply.github.com>
2022-09-20 22:24:33 +0000

    Added new response

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 def generate_response(user_input):
     responses = [
         "How interesting!", "You don't say!", "Very cool!",
-        "Programming is fun!"
+        "Programming is fun!", "Tell me more!"
     ]
     return random.choice(responses)
 
